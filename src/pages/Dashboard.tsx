9e531543f69285d5704df277f4927215c5eadb0a
Aman Sanoj <amansanoj08@gmail.com>
2025-06-09 19:58:44 +0400

    Updated Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -295,7 +295,7 @@ export default function Dashboard() {
       </div>
 
       {/* Stats Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols43 xl:grid-cols-4 gap-4 sm:gap-6">
         {getStatsCards()}
       </div>
 
