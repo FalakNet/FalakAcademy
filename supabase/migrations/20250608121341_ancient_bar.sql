257f3be1f3727ae0b6f11f4a116f2101fc85800a
Aman Sanoj <amansanoj08@gmail.com>
2025-06-09 19:57:17 +0400

    Deleted 20250608082704_wandering_feather.sql

--- supabase/migrations/20250608121341_ancient_bar.sql
+++
@@ -1,4 +0,0 @@
--- This file allows to write SQL commands that will be executed when the database is reset.
--- Here you can insert default data that you want to have in your database when you start developing.
-
--- Insert default data here if needed
